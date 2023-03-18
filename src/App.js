b37be9aa009f4d672100aa3dc665f48b4f9e2cc8
Justin  Cambria <justincambria@Justins-MacBook-Air.local>
2023-03-18 13:17:12 -0400

    started style

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 import { Footer, Blog, Features, Title, Header } from './containers';
 import { CTA, Navbar } from './components';
+import './App.css';
+
 
 const App = () => {
   return (
